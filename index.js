6bac7261daf4750acb7dfd497fba6fd58bab2db6
doswald32 <dan.oswald32@gmail.com>
2024-01-16 21:45:43 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let employeeRecord = ["Dan", "Oswald", "Sales", 20]
-
 function createEmployeeRecord(array) {
     let obj = {
         "firstName": array[0],
@@ -12,13 +10,6 @@ function createEmployeeRecord(array) {
     return obj;
 };
 
-console.log(createEmployeeRecord(employeeRecord));
-
-let twoRows = [
-    ["moe", "sizlak", "barkeep", 2],
-    ["bartholomew", "simpson", "scamp", 3]
-  ];
-
 function createEmployeeRecords(arrayOfArrays) {
     let arrayOfObjs = [];
     for (let i = 0; i < arrayOfArrays.length; i++) {
@@ -28,8 +19,59 @@ function createEmployeeRecords(arrayOfArrays) {
     return arrayOfObjs;
 };
 
-console.log(createEmployeeRecords(twoRows));
+function createTimeInEvent(eRecord, date) {
+    let timeInEvent = {
+        "type": "TimeIn",
+        "hour": parseInt(date.slice(-4)),
+        "date": date.slice(0, 10)
+    };
+    eRecord.timeInEvents.push(timeInEvent);
+    return eRecord;
+};
 
-function createTimeInEvent() {
-    
-}
+function createTimeOutEvent(eRecord, date) {
+    let timeOutEvent = {
+        "type": "TimeOut",
+        "hour": parseInt(date.slice(-4)),
+        "date": date.slice(0, 10)
+    };
+    eRecord.timeOutEvents.push(timeOutEvent);
+    return eRecord;
+};
+
+function hoursWorkedOnDate(eRecord, date) {
+    let timeInEvent = eRecord.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = eRecord.timeOutEvents.find(event => event.date === date);
+  
+    let timeOut = timeOutEvent.hour;
+    let timeIn = timeInEvent.hour;
+  
+    let hours = (timeOut - timeIn) / 100;
+    return hours;
+  };
+
+  function wagesEarnedOnDate(eRecord, date) {
+    let hours = hoursWorkedOnDate(eRecord, date);
+    let payRate = parseInt(eRecord.payPerHour);
+    let pay = hours * payRate;
+    return pay;
+  };
+
+  function allWagesFor(eRecord) {
+    let totalWages = 0;
+    for (let i = 0; i < eRecord.timeInEvents.length; i++) {
+        let daysWage = wagesEarnedOnDate(eRecord, eRecord.timeInEvents[i].date);
+        totalWages += daysWage;
+    };
+    return totalWages;
+  };
+
+//   function calculatePayroll(array) {
+//     let grandTotal = array.forEach((employee) => allWagesFor(employee));
+//     return grandTotal;
+//   };
+
+  function calculatePayroll(array) {
+    let grandTotal = array.reduce((total, employee) => total + allWagesFor(employee), 0);
+    return grandTotal;
+  }
